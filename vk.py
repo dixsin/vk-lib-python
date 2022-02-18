882d0ada5c1683788d18504c8862640e0aaa0641
Dixsin <66322670+dixsin@users.noreply.github.com>
2022-02-18 09:34:54 +0300

    Подключение

--- vk.py
+++ vk.py
@@ -10,6 +10,6 @@ session = vk.Session()
 vk_api = vk.API(session)
 vk_api.users.get(user_id=id)
 session = vk.Session(access_token=token)
-print('Выполнено подключение к VK Api...')
+print('Выполнено подключение к VK Api!')
 
 vk_api.messages.send(users_id=0, messages=’hello’)
