1dae31c676838f090ca28f09ab1afdf7a2ce4d5c
Dixsin <66322670+dixsin@users.noreply.github.com>
2022-02-18 09:34:30 +0300

    Основа кода

--- vk.py
+++ vk.py
@@ -1,1 +1,15 @@
+import vk
 
+# Ваш цифровой ID VK
+id = 1
+# Ваш токен VK
+token = 'token'
+
+print('Выполняется подключение к VK Api...')
+session = vk.Session()
+vk_api = vk.API(session)
+vk_api.users.get(user_id=id)
+session = vk.Session(access_token=token)
+print('Выполнено подключение к VK Api...')
+
+vk_api.messages.send(users_id=0, messages=’hello’)
