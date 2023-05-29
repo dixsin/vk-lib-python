14ae4b80c6987bd6d1cec82674d5f3ffae258ec3
Dixsin <66322670+dixsin@users.noreply.github.com>
2023-05-29 08:19:01 +0300

    Update vk.py
    Бла бля бла очень много изминений)))

--- vk.py
+++ vk.py
@@ -1,9 +1,8 @@
 import vk
 
-# Ваш цифровой ID VK
-id = 1
-# Ваш токен VK
-token = 'token'
+# Данные для входа в вк чат
+id = 1 # Айди для верификации чата ВК при использовании LongPool
+token = 'token' # Ваш персональный токен ВК
 
 print('Выполняется подключение к VK Api...')
 session = vk.Session()
@@ -12,7 +11,7 @@ vk_api.users.get(user_id=id)
 session = vk.Session(access_token=token)
 print('Выполнено подключение к VK Api!')
 
-id_user = input('Введите ID пользователя которому будете писать сообщения >>> ')
+id_user = 1 # ID пользователя которому будет отправлено сообщение
 while true:
   message_from_user = input('Ваше сообщение пользователю >>> ')
   vk_api.messages.send(users_id=id_user, messages=message_from_user)
